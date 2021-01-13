c2b625a3bdb473e424a645e3e6a768749b7ca718
Anshuman Aich <anshuman.aich@bacancy.com>
2021-01-13 15:08:14 +0530

    saves to file

--- server/server.go
+++ server/server.go
@@ -1,23 +1,45 @@
 package main
 
 import (
+  "os"
   "fmt"
+  "log"
+  "encoding/json"
   "net/http"
 )
 
 type Employee struct {
-  name, email, password, postion string
+  Name, Email, Password, Position string
 }
 
-func defaultRoute(res http.ResponseWriter, req *http.Request) {
+func read() {
+}
+
+func save(emp *Employee) {
+  openFile, openFileErr := os.OpenFile("EmployeeMaster.json", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
+  if openFileErr != nil {
+    log.Fatal(openFileErr)
+  }
+  empData, fileErr := json.Marshal(emp)
+  if fileErr != nil {
+    fmt.Println(fileErr)
+    return
+  }
+  _, _ = openFile.WriteString(string(empData) + "\n")
+}
+
+func addEmp(res http.ResponseWriter, req *http.Request) {
   name := req.FormValue("name")
   email := req.FormValue("email")
   pos := req.FormValue("pos")
   pass := req.FormValue("password")
-  fmt.Fprintf(res, "Name: %s\nEmail: %s\nPosition: %s\nPassword: %s", name, email, pos, pass)
+  //fmt.Fprintf(res, "Name: %s\nEmail: %s\nPosition: %s\nPassword: %s", name, email, pos, pass)
+
+  e := &Employee{Name: name, Email: email, Password: pass, Position: pos}
+  save(e)
 }
 
 func main() {
-  http.HandleFunc("/", defaultRoute)
+  http.HandleFunc("/add", addEmp)
   http.ListenAndServe(":8000", nil)
 }

fbcaf5d7dc13f3257d02fa129bbae612415a6ab0
Anshuman Aich <anshuman.aich@bacancy.com>
2021-01-16 23:40:49 +0530

    refactored. added routes

--- server/server.go
+++ server/server.go
@@ -4,11 +4,16 @@ import (
   "fmt"
   "net/http"
   "io/ioutil"
-  //"text/template"
+  "html/template"
   "encoding/json"
 )
 
 var empFilePath string = "data/EmployeeMaster.json"
+
+type Dashboard struct {
+  AllEmps []Employee
+  AdminCreds Admin
+}
 
 type Employee struct {
   Name string `json:"Name"`
@@ -21,6 +26,8 @@ type Admin struct {
   Email, Password string
 }
 
+// ----------- HELPER FUNCTIONS ---------------
+
 func getEmployees() []Employee {
   file, _ := ioutil.ReadFile(empFilePath)
   var allEmps []Employee
@@ -29,32 +36,91 @@ func getEmployees() []Employee {
   return allEmps
 }
 
-func handleAdmin(res http.ResponseWriter, req *http.Request) {
-  email, _ := req.FormValue("email"), req.FormValue("password")
-  fmt.Fprintf(res, "Welcome, %s \n", email)
-}
-
 func save(emp Employee) {
   allEmps := getEmployees()
   allEmps = append(allEmps, emp)
   empMarshalInd, _ := json.MarshalIndent(allEmps, "", "")
   _ = ioutil.WriteFile(empFilePath, empMarshalInd, 0644)
 }
+// ------------------------------------------
 
-func register(res http.ResponseWriter, req *http.Request) {
-  name := req.FormValue("name")
-  email := req.FormValue("email")
-  pos := req.FormValue("pos")
-  pass := req.FormValue("password")
 
-  e := Employee{Name: name, Email: email, Password: pass, Position: pos}
-  save(e)
+
+func admin(res http.ResponseWriter, req *http.Request) {
+  if req.Method == "GET" {
+    adminTemp := template.Must(template.ParseFiles("static/admin.html"))
+    adminTemp.Execute(res, nil)
+  }
+
+  if req.Method == "POST" {
+    email, pass := req.FormValue("email"), req.FormValue("password")
+
+    adminCreds := Admin{Email: email, Password: pass}
+    dashboard := Dashboard{AllEmps: getEmployees(), AdminCreds: adminCreds}
+
+    adminTemp := template.Must(template.ParseFiles("static/dashboard.html"))
+    //allEmps := getEmployees()
+    adminTemp.Execute(res, dashboard)
+
+    //fmt.Fprintf(res, "Welcome, %s \n", email)
+
+    //disp := template.Must(template.ParseFiles("static/adminHome.html"))
+    //disp.Execute(res, allEmps)
+
+    //http.Redirect(res, req, "static/adminHome.html", http.StatusSeeOther)
+    /*
+    fmt.Fprintf(res, "Welcome, %s \n", email)
+
+
+    disp := template.Must(template.ParseFiles("static/adminHome.html"))
+    //disp, _ := template.ParseFiles("static/admin.html")
+    disp.Execute(res, allEmps)
+    */
+  }
+}
+
+func employee(res http.ResponseWriter, req *http.Request) {
+  if req.Method == "GET"  {
+    empTemp := template.Must(template.ParseFiles("static/reg.html"))
+    empTemp.Execute(res, nil)
+  }
+  if req.Method == "POST" {
+    name := req.FormValue("name")
+    email := req.FormValue("email")
+    pos := req.FormValue("pos")
+    pass := req.FormValue("password")
+
+    e := Employee{Name: name, Email: email, Password: pass, Position: pos}
+    save(e)
+
+    empTemp := template.Must(template.ParseFiles("static/reg.html"))
+    empTemp.Execute(res, nil)
+  }
+}
+
+func showEmployees(res http.ResponseWriter, req *http.Request) {
+  if req.Method == "GET" {
+    //email, _ := req.FormValue("email"), req.FormValue("password")
+    allEmps := getEmployees()
+
+    disp := template.Must(template.ParseFiles("static/adminHome.html"))
+    disp.Execute(res, allEmps)
+  }
+}
+
+func home(res http.ResponseWriter, req *http.Request) {
+  if req.Method == "GET" {
+    homeTemp := template.Must(template.ParseFiles("static/home.html"))
+    homeTemp.Execute(res, nil)
+  }
 }
 
 func main() {
   fmt.Println("\033[32mServer is running....")
 
-  http.HandleFunc("/admin", handleAdmin)
-  http.HandleFunc("/register", register)
+  http.HandleFunc("/home", home)
+  http.HandleFunc("/admin", admin)
+  http.HandleFunc("/employee", employee)
+  http.HandleFunc("/showEmployees", showEmployees)
   http.ListenAndServe(":8000", nil)
 }

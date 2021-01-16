d50730b12bfce0c1700dda3654c6acb3b7932924
Anshuman Aich <anshuman.aich@bacancy.com>
2021-01-16 19:45:53 +0530

    minor code changes

--- server/server.go
+++ server/server.go
@@ -1,14 +1,10 @@
 package main
 
 import (
-  "io"
-  "os"
   "fmt"
-  "log"
-  "bytes"
-  "strings"
   "net/http"
   "io/ioutil"
+  //"text/template"
   "encoding/json"
 )
 
@@ -16,9 +12,9 @@ var empFilePath string = "data/EmployeeMaster.json"
 
 type Employee struct {
   Name string `json:"Name"`
-  Email string`json:"Email"`
-  Password string`json:"Password"`
-  Position string`json:"Position"`
+  Email string `json:"Email"`
+  Password string `json:"Password"`
+  Position string `json:"Position"`
 }
 
 type Admin struct {
@@ -27,50 +23,22 @@ type Admin struct {
 
 func getEmployees() []Employee {
   file, _ := ioutil.ReadFile(empFilePath)
-  empList := strings.Split(string(file), "\n")
-  var e Employee
   var allEmps []Employee
+  json.Unmarshal([]byte(file), &allEmps)
 
-  for _, emp := range empList {
-    if emp == "" { break }
-    json.Unmarshal([]byte(emp), &e)
-    allEmps = append(allEmps, e)
-  }
   return allEmps
 }
 
 func handleAdmin(res http.ResponseWriter, req *http.Request) {
   email, _ := req.FormValue("email"), req.FormValue("password")
   fmt.Fprintf(res, "Welcome, %s \n", email)
-
-  employees := getEmployees()
-  for _, em := range employees {
-    fmt.Println(em.Name)
-  }
 }
 
 func save(emp Employee) {
-  buffer := new(bytes.Buffer)
-  encoder := json.NewEncoder(buffer)
-  encoder.Encode(emp)
-
-  openFile, openFileErr := os.OpenFile(empFilePath, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
-
-  defer openFile.Close()
-
-  if openFileErr != nil {
-    log.Fatal(openFileErr)
-  }
-
-  io.Copy(openFile, buffer)
-
-  /*
-
-  // marshaling(struct to JSON)
-  empMarshalInd, _ := json.MarshalIndent(emp, "", "")
-  empMarshalIndStr := string(empMarshalInd)
-  _, _ = openFile.WriteString(empMarshalIndStr + "\n")
-  */
+  allEmps := getEmployees()
+  allEmps = append(allEmps, emp)
+  empMarshalInd, _ := json.MarshalIndent(allEmps, "", "")
+  _ = ioutil.WriteFile(empFilePath, empMarshalInd, 0644)
 }
 
 func register(res http.ResponseWriter, req *http.Request) {
@@ -84,8 +52,9 @@ func register(res http.ResponseWriter, req *http.Request) {
 }
 
 func main() {
+  fmt.Println("\033[32mServer is running....")
+
   http.HandleFunc("/admin", handleAdmin)
   http.HandleFunc("/register", register)
   http.ListenAndServe(":8000", nil)
-  fmt.Println("\033[32m Server is running....")
 }
